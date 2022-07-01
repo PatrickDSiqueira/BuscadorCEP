6164c13731c9d820c61a217c40fd66f96d7f1c49
Patrick Siqueira <psique.estuda@gmail.com>
2022-07-01 20:09:28 -0300

    import de pagina de estilos e html antigo

--- buscadorcep/src/Home/Home.js
+++ buscadorcep/src/Home/Home.js
@@ -1,7 +1,43 @@
+import '../Home/style.css';
+
+
 export function Home() {
     return (
         <div>
-            Hello World!
+            <divBody>
+    <div class="container">
+        <form >
+            <h1 id="titulo">Consultando CEP</h1>
+            <div class="form-group">
+                <label for="cep">CEP</label><br/>
+                <input type="text" class="form_control" id="cep" maxlength="9"/><br/>
+                <small /*style="font-size: 10px;"*/>00000-000</small>
+            </div>
+            <br/>
+            <div class="form-group">
+                <label for="logradouro">Logradouro</label><br/>
+                <input type="text" class="form_control" id="logradouro"/><br/>
+                <small>Rua</small>
+            </div><br/>
+            <div class="form-group">
+                <label for="bairro">Bairro</label><br/>
+                <input type="text" class="form_control" id="bairro"/><br/>
+                <small>Bairro</small>
+            </div><br/>
+            <div class="form-group">
+                <label for="localidade">Localidade</label><br/>
+                <input type="text" class="form_control" id="localidade"/><br/>
+                <small>Cidade</small>
+            </div><br/>
+            <div class="form-group">
+                <label for="uf">UF</label><br/>
+                <input type="text" class="form_control" id="uf"/><br/>
+                <small>Estado</small>
+            </div>
+        </form> 
+    </div>
+    <script src="main.js"></script>
+</divBody>
         </div>
     )
 }
